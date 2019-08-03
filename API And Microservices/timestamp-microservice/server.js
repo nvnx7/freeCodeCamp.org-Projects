6fbd61e8aba654f104e1917bf60848384f3564a9
Naveen <thenvnsahu@gmail.com>
2019-08-03 20:47:56 +0530

    completed timestamp microservice project

---
+++ API And Microservices/timestamp-microservice/server.js
@@ -0,0 +1,47 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// API endpoint handling for timestamp
+app.get("/api/timestamp/:date_string?", (req, res) => {
+  var dateParam = req.params.date_string;
+  if (!isNaN(dateParam)) {
+    dateParam = Number(dateParam);
+  }
+
+  var d;
+  if (dateParam == "" || dateParam == undefined) {
+    d = new Date();
+  } else {
+    d = new Date(dateParam);
+  }
+
+  if (isNaN(d.getTime())) {
+    res.json({"error": "Invalid Date"});
+  } else {
+    res.json({"unix": d.getTime(), "utc": d.toUTCString()});
+  }
+  
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
